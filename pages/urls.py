db698aeefda6a8e0931b8ad5c304cd4332999e97
kchateau <chateau14@outlook.com>
2020-01-14 22:06:01 -0800

    Display logged in user's name and profile picture
    When a user logs in it now displays their steam avatar and id, and name.

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import HomePageView
 
+
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('home', views.LoginView.as_view(), name='on_login'),
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from steam.webapi import WebAPI
+import os
 
 
 class HomePageView(TemplateView):
@@ -7,3 +9,10 @@ class HomePageView(TemplateView):
 
 class LoginView(TemplateView):
     template_name = 'on_login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        api = WebAPI(os.environ.get("STEAM_KEY"))
+        response = api.call('ISteamUser.GetPlayerSummaries', steamids=self.request.user.steam_id)
+        context["response"] = response["response"]["players"][0]
+        return context
