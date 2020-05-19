23867f40f49152d2c6c71218586efe94a29b6a38
kchateau <chateau14@outlook.com>
2020-05-19 16:40:00 -0700

    Add current user state
    Added a text showing if the user is online, offline, away, etc.

--- pages/views.py
+++ pages/views.py
@@ -10,9 +10,29 @@ class HomePageView(TemplateView):
 class LoginView(TemplateView):
     template_name = 'on_login.html'
 
+    def get_state(self, stateNum):
+        if stateNum == 0:
+            return "Offline"
+        elif stateNum == 1:
+            return "Online"
+        elif stateNum == 2:
+            return "Busy"
+        elif stateNum == 3:
+            return "Away"
+        elif stateNum == 4:
+            return "Snooze"
+        elif stateNum == 5:
+            return "Looking to Trade"
+        elif stateNum == 6:
+            return "Looking to Play"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         api = WebAPI(os.environ.get("STEAM_KEY"))
         response = api.call('ISteamUser.GetPlayerSummaries', steamids=self.request.user.steam_id)
+        state = self.get_state(response["response"]["players"][0]["profilestate"])
+        response["response"]["players"][0]["profilestate"] = state
         context["response"] = response["response"]["players"][0]
         return context
+
+    
