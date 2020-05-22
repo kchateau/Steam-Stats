c2b1956acc8c5d14f8d585688728ab679b2dde81
kchateau <chateau14@outlook.com>
2020-05-22 13:55:19 -0700

    Convert unix timestamp of friend_since to datetime.
    Friend_since displays as month, day, year instead of a unix timestamp.

--- lib/steam_response_objects/friend.py
+++ lib/steam_response_objects/friend.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Friend:
     steam_id = ""
     relationship = ""
@@ -6,7 +8,11 @@ class Friend:
     def __init__(self, steam_id, relationship, friend_since):
         self.steam_id = steam_id
         self.relationship = relationship
-        self.friend_since = friend_since
+        self.friend_since = self.convert_friend_since(friend_since)
+
+    def convert_friend_since(self, friend_since):
+        return datetime.datetime.utcfromtimestamp(friend_since).strftime('%m-%d-%Y')
+
 
     
 
